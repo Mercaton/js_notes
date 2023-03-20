d9dc9a89f23b2221b7d369bdff685eb034d04a2a
mercaton <m.wieczorek001@wp.pl>
2023-03-20 10:20:52 +0100

    js variables notes

---
+++ src/scripts/app.js
@@ -0,0 +1,34 @@
+// Redeklaracja
+
+// var x = 42;
+// let x1 = 42;
+// const x2 = 42;
+//
+// console.log(x, x1, x2);
+//
+// var x = 42;
+// let x1 = 42;
+// const x2 = 42;
+
+//
+// Reinicjalizacja
+//
+// var x = 42;
+// let x1 = 42;
+// const x2 = 42;
+//
+// x = 666; //ok
+// x1 = 666; //ok
+// x2 = 666; //not
+//
+// console.log(x, x1, x2);
+
+// Hoisting - przeniesienie deklaracji zmiennej, funkcji, klasy na górę aktualnie przetwarzanego zasięgu
+// var, let, const - są hoistowane, ale let i const trafiają do TDZ
+
+// Scope, browser - window (global object), nodejs - object
+// zasięg funkcyjny - tworzony przez funkcję
+// zasięg blokowy - tworzony przez parę klamerek {
+
+
+
